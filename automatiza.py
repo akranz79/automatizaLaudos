74ca589755fc9d1aba54e421ab40a99ca2af0fab
Alexandre Kranz <ahkranz79@gmail.com>
2024-11-15 13:39:46 -0300

    alteração automatiza.py

--- automatiza.py
+++ automatiza.py
@@ -6,8 +6,38 @@ from datetime import datetime
 from logger import gerar_log
 import json
 
+
 def enviar_arquivos_email(config):
     os.system('python enviaEmail.py')
+
+
+def gerar_laudos_com_retry():
+    """
+    Chama a função 'gerar_laudos_pendentes' com tentativas de reconexão em caso de falha,
+    com intervalos progressivos de espera entre as tentativas.
+    """
+    attempt = 1  # Contador de tentativas
+
+    while True:
+        try:
+            gerar_laudos_pendentes()
+            break  # Sai do loop se a chamada for bem-sucedida
+        except Exception as e:
+            # Define o intervalo de retry conforme a tentativa
+            if attempt == 1:
+                retry_delay = 10
+            elif attempt == 2:
+                retry_delay = 20
+            elif attempt == 3:
+                retry_delay = 30
+            else:
+                retry_delay = 50
+
+            print(f"Erro ao acessar Google Sheets (tentativa {attempt}): {e}")
+            print(f"Tentando novamente em {retry_delay} segundos...")
+            time.sleep(retry_delay)
+            attempt += 1
+
 
 def automatizar(config):
     while True:
@@ -16,13 +46,13 @@ def automatizar(config):
         current_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
         # Printar mensagem
-        print(f"                                                  ")
-        log4 = (f"Automatizando geração de laudos - ({current_time})")
-        print(f"                                                  ")
+        print("                                                  ")
+        log4 = f"Automatizando geração de laudos - ({current_time})"
+        print("                                                  ")
         gerar_log(log4, config)
 
-        # Chamar função para gerar laudos pendentes
-        gerar_laudos_pendentes()
+        # Chamar função para gerar laudos pendentes com lógica de retry
+        gerar_laudos_com_retry()
 
         # Aguardar 5 segundos
         time.sleep(2)
@@ -31,15 +61,14 @@ def automatizar(config):
         os.system('python criaGraficoGeraImagem.py')
         time.sleep(1)
 
-        # Chamar a função para inserir a imagem criada no paragrafo 42 do laudo
-        os.system('inserirImgPar42.py')
+        # Chamar a função para inserir a imagem criada no parágrafo 42 do laudo
+        os.system('python inserirImgPar42.py')
         time.sleep(1)
 
         # Aguardar 5 segundos
         time.sleep(1)
 
         # Chamar função do arquivo moveImagemParaPastaImgLaudo.py
-        #print("Movendo imagens para pasta imgLaudos [load...]")
         os.system('python moveImagemParaPastaImgLaudo.py')
 
         # Aguardar 5 segundos
@@ -49,13 +78,14 @@ def automatizar(config):
         os.system('python removeTabelaLaudo.py')
 
         # Printar mensagem
-        print(f"Aguardando {config['tempo_loop']/60} minutos para próxima verificação.")
+        print(f"Aguardando {config['tempo_loop'] / 60} minutos para próxima verificação.")
 
         # Chamar função para enviar arquivos por email, passando o argumento config
         enviar_arquivos_email(config)
 
         # Aguardar o tempo configurado no arquivo de configuração para execução da próxima iteração do while
         time.sleep(config['tempo_loop'])
+
 
 if __name__ == "__main__":
     with open('config.json') as f:
